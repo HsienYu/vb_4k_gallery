82c7c5b63fba08e91916ca092d7804b5460d4529
H.Y.C <chenghsienyu@aim.com>
2019-03-07 01:34:58 +0800

    fixed process issue

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const config = require('./config.json');
 var sref_a = null;
 var sref_v = null;
 
-process.setMaxListeners(Infinity); // <== Important line
-
 const delay = (interval) => {
     return new Promise((resolve) => {
         setTimeout(resolve, interval);
@@ -14,11 +12,20 @@ const delay = (interval) => {
 }
 const Start = async () => {
 
+    //process.setMaxListeners(Infinity); // <== Important line
+
     while (true) {
 
         if (sref_a == null) {
             let call = `gst-launch-1.0 ${config.audio_path} ! audioconvert ! lamemp3enc ! shout2send ip=127.0.0.1 port=8000 password=tinker mount=tinker`
-            sref_a = exec(call);
+            sref_a = exec(call, (error, stdout, stderr) => {
+                if (error) {
+                    //console.error(`exec error: ${error}`);
+                    return;
+                }
+                //console.log(`stdout: ${stdout}`);
+                //console.log(`stderr: ${stderr}`);
+            });
             console.log('stream audio start');
         }
 
@@ -34,7 +41,14 @@ const Start = async () => {
 
         if (sref_v == null) {
             let call = `gst-play-1.0 --videosink=kmssink ${config.video_path}`;
-            sref_v = exec(call);
+            sref_v = exec(call, (error, stdout, stderr) => {
+                if (error) {
+                    //console.error(`exec error: ${error}`);
+                    return;
+                }
+                //console.log(`stdout: ${stdout}`);
+                //console.log(`stderr: ${stderr}`);
+            });
             console.log('video start');
         }
 
