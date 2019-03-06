a4405648efe09d269f137fca5bff4a66e6ec9478
H.Y.C <chenghsienyu@aim.com>
2019-03-07 00:34:31 +0800

    change order of functions
    change order of functions

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const exec = require('child_process').exec;
 const kill = require('tree-kill');
 const config = require('./config.json');
 
-var sref_v = null;
 var sref_a = null;
+var sref_v = null;
 
 process.setMaxListeners(Infinity); // <== Important line
 
@@ -15,21 +15,6 @@ const delay = (interval) => {
 const Start = async () => {
 
     while (true) {
-        if (sref_v == null) {
-            let call = `gst-play-1.0 --videosink=kmssink ${config.video_path}`;
-            sref_v = exec(call);
-            console.log('video start');
-        }
-
-        sref_v.on('close', (code) => {
-            //console.log('video_exit');
-            kill(sref_v.pid, 'SIGTERM', function () {
-                //console.log('Killed ', sref_v.pid);
-                sref_v = null;
-            });
-        });
-
-        await delay(config.time);
 
         if (sref_a == null) {
             let call = `gst-launch-1.0 ${config.audio_path} ! audioconvert ! lamemp3enc ! shout2send ip=127.0.0.1 port=8000 password=tinker mount=tinker`
@@ -42,6 +27,22 @@ const Start = async () => {
             kill(sref_a.pid, 'SIGTERM', function () {
                 //console.log('Killed ', sref_a.pid);
                 sref_a = null;
+            });
+        });
+
+        await delay(config.time);
+
+        if (sref_v == null) {
+            let call = `gst-play-1.0 --videosink=kmssink ${config.video_path}`;
+            sref_v = exec(call);
+            console.log('video start');
+        }
+
+        sref_v.on('close', (code) => {
+            //console.log('video_exit');
+            kill(sref_v.pid, 'SIGTERM', function () {
+                //console.log('Killed ', sref_v.pid);
+                sref_v = null;
             });
         });
 
