0dfa66b1032727f17f5e53e2c0da20af3abb4f71
H.Y.C <chenghsienyu@aim.com>
2019-03-09 23:03:12 +0800

    remove while loop

--- app.js
+++ app.js
@@ -16,48 +16,45 @@ const Start = async () => {
     let call = 'pacmd set-default-sink alsa_output.OnBoard_D2'
     exec(call);
 
-    while (true) {
 
-        if (sref_a == null) {
-            let call = `gst-launch-1.0 ${config.audio_path} ! audioconvert ! lamemp3enc ! shout2send ip=127.0.0.1 port=8000 password=tinker mount=tinker`
-            //"pulsesrc device=alsa_output.OnBoard_D2.monitor"//audio from system output
-            sref_a = exec(call, (error, stdout, stderr) => {
-                if (error) {
-                    //console.error(`exec error: ${error}`);
-                    return;
-                }
-                //console.log(`stdout: ${stdout}`);
-                //console.log(`stderr: ${stderr}`);
-            });
-            console.log('stream audio start');
-        }
-
-        sref_a.on('close', (code) => {
-            //console.log('audio_exit');
-            sref_a = null;
+    if (sref_a == null && sref_v == null) {
+        let call = `gst-launch-1.0 ${config.audio_path} ! audioconvert ! lamemp3enc ! shout2send ip=127.0.0.1 port=8000 password=tinker mount=tinker`
+        //"pulsesrc device=alsa_output.OnBoard_D2.monitor"//audio from system output
+        sref_a = exec(call, (error, stdout, stderr) => {
+            if (error) {
+                //console.error(`exec error: ${error}`);
+                return;
+            }
+            //console.log(`stdout: ${stdout}`);
+            //console.log(`stderr: ${stderr}`);
         });
-
-        await delay(config.tol);
-
-        if (sref_v == null) {
-            let call = `gst-play-1.0 --videosink=kmssink ${config.video_path}`;
-            sref_v = exec(call, (error, stdout, stderr) => {
-                if (error) {
-                    //console.error(`exec error: ${error}`);
-                    return;
-                }
-                //console.log(`stdout: ${stdout}`);
-                //console.log(`stderr: ${stderr}`);
-            });
-            console.log('video start');
-        }
-
-        sref_v.on('close', (code) => {
-            //console.log('video_exit');
-            sref_v = null;
-        });
-
+        console.log('stream audio start');
     }
+
+    await delay(config.tol);
+
+    if (sref_v == null) {
+        let call = `gst-play-1.0 --videosink=kmssink ${config.video_path}`;
+        sref_v = exec(call, (error, stdout, stderr) => {
+            if (error) {
+                //console.error(`exec error: ${error}`);
+                return;
+            }
+            //console.log(`stdout: ${stdout}`);
+            //console.log(`stderr: ${stderr}`);
+        });
+        console.log('video start');
+    }
+
+    sref_a.on('close', (code) => {
+        //console.log('audio_exit');
+        sref_a = null;
+    });
+
+    sref_v.on('close', (code) => {
+        //console.log('video_exit');
+        sref_v = null;
+    });
 }
 
 const Stop = () => {
@@ -76,3 +73,5 @@ const Stop = () => {
 }
 
 Start();
+
+setInterval(Start, 500);
